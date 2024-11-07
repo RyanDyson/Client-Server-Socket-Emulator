745b985c4862d473e4728b4ed2bdf765c10b9f50
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2024-11-07 20:59:24 +0800

    finished

--- MessageBoardClient.py
+++ MessageBoardClient.py
@@ -1,56 +1,91 @@
+#Ryan Dyson Darmawan - 57883349
+
 from socket import *
 import sys
+import signal
+
+clientSocket = None #initialize clientSocket to None
+
+def signal_handler(sig, frame):  #handles when client force quites via terminal (CTRL + C)
+    clientSocket.send("QUIT".encode())
+    isOk = clientSocket.recv(1024).decode()
+    if clientSocket and isOk == "OK":
+        clientSocket.close()
+        print("\nClient: Socket forcefully closed. Exiting program.")
+        sys.exit(0)
+
 
 def main(ipAddress, serverPortNum):
-    def handleCommand(Command):
-        clientSocket.send(Command.encode())
+    global clientSocket #access nonlocal variable
 
-        if (Command[0] == "POST"):
-            exited = False
-            while not exited:
-                newMsg = input("Client, enter a message: ").strip()
-                if (input == "#\n"):
-                    exited = True
+    def handleCommand(Command): #helper function to handle commands
+        clientSocket.send(Command.encode()) #send command to server
+
+        if (Command == "POST"):
+            isDone = False
+            while not isDone: #loops while user still want to post
+                newMsg = input("Client, enter a message: ").strip() #get user input
+                clientSocket.send(newMsg.encode()) #set user input that is encoded
+                if (newMsg == "#"): #if input == #, break the loop
+                    isDone = True
                     break
-                clientSocket.send(newMsg.encode())
+            isOk = clientSocket.recv(1024).decode() #check response from server
+            if (isOk == "OK"):
+                print("Server: OK, Message added successfully")
+            else: 
+                print("Server: ERROR - Message was not received")
 
-        elif (Command[0] == "GET"):
-            happySocket = clientSocket.recv(1024).decode()
+        elif (Command == "GET"):
+            happySocket = clientSocket.recv(1024).decode() #get the "happy socket programming" response from server
             print("Server: ", happySocket)
             isDone = False
-            while not isDone:
-                currMessage = clientSocket.recv(1024).decode()
-                if (currMessage == "#\n"):
+            while not isDone: #while there is still messages to get
+                currMessage = clientSocket.recv(1024).decode() #first response is the ID and date
+                if (currMessage == "#"): #break if response is #
                     isDone = True
                     break
 
                 print("server: ", currMessage)
 
-        elif (Command[0] == "DELETE"):
+        elif (Command == "DELETE"):
             isDone = False
             while not isDone:
-                messageID = input("Client, enter a message ID: ").strip()
-                if (messageID == "#\n"):
+                messageID = input("Client, enter a message ID: ").strip() #get the IDs to delete
+                clientSocket.send(messageID.encode()) #send the ID to server
+                if (messageID == "#"): #break if user is done
                     isDone = True
                     break
-                clientSocket.send(messageID.encode())
+
+            isOk = clientSocket.recv(1024).decode() #get response from server
+            if (isOk != "OK"): #check the response
+                print("Server: ERROR - Wrong ID")
+            else:
+                print("Server: Message(s) deleted successfully")
 
         elif (Command == "QUIT"):
-            clientSocket.close()
-            sys.exit(0)
+            isOk = clientSocket.recv(1024).decode() #get response from server
+            if (isOk == "OK"):
+                clientSocket.close()
+                sys.exit(0)
 
-        else:
-            serverRes = clientSocket.recv(1024).decode()
+        else: #if command is not reconized
+            serverRes = clientSocket.recv(1024).decode() #server will send our error message
             print("Server: ", serverRes)
 
     serverName = ipAddress
     serverPort = serverPortNum
 
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
+    clientSocket = socket(AF_INET, SOCK_STREAM) #initialize TCP socket
+    try:
+        clientSocket.connect((serverName, serverPort)) #try to connect to server of specified IP and Port num
+    except ConnectionRefusedError:
+        print("Client: Failed connecting to server. Exiting program.") #if cannot find server, close the program
+        if (clientSocket):
+            clientSocket.close()
+        sys.exit(1)
 
-    while True:
-        command = input("Client, enter a command: ").strip().upper().split(" ")
+    while True: 
+        command = input("Client, enter a command: ").strip().upper() #get command from user
         handleCommand(command)
         
 
@@ -61,4 +96,8 @@ if __name__ == "__main__": #dunder method to execute main func
     
     serverIp = sys.argv[1]
     serverPort = int(sys.argv[2])
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     main(serverIp, serverPort) #calls main func to handle ther client
--- README.md
+++ README.md
@@ -8,4 +8,16 @@
 <li>This program was written in python, the corresponding server code is provided by the course and university and is not a part of the assignment</li>
 </ul>
 
-## Compiling
+## Dev Env
+
+This program was written in Python 3.12.6 and in VSC, windows 11.
+
+## How To Run
+
+You can run the python file via the terminal. Start by CD-ing in the directory the files are stored.
+
+then simply type **py MessageBoardClient.py < IP address > < Server Port Num >**
+
+in this case you can use
+IP address = 127.0.0.1
+server Port num = 16111
